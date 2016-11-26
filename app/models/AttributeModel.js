6fc1ee38f5e6386f83cbcdc332314a93407aef15
tmfrnz <timo@dumpark.com>
2016-11-26 18:12:41 +1300

    pass query parameter

--- app/models/AttributeModel.js
+++ app/models/AttributeModel.js
@@ -6,10 +6,14 @@ define([
   return Backbone.Model.extend({
     initialize : function(options) {
       this.options = options || {};    
-        
- 
+      if (typeof this.get('query') === "undefined" && this.get("combo") !== 1) {
+        this.set("query",this.get("column"))
+      } 
 
-    } 
+    },
+    getQueryAttribute : function(){
+      return this.attributes.query
+    }
   });  
 
 });
