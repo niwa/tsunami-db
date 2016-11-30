5f09c6d5593ec1d7c2e696d10f87e48729df80fd
tmfrnz <timo@dumpark.com>
2016-11-30 13:14:12 +1300

    update optimiser settings

--- app/app.build.js
+++ app/app.build.js
@@ -4,19 +4,13 @@
   dir: "../../build",
   paths: {
     'domReady' : 'libs/domReady',    
-    'jquery': 'libs/jquery',
+    'jquery': 'libs/jquery-1.11.2',
     'jquery.deparam': 'libs/jquery.deparam',
-    'jquery.dateformat': 'libs/jquery-dateFormat',    
-    'jquery.waitforimages': 'libs/jquery.waitforimages',    
-    'showdown': 'libs/showdown', //markdown support
     'underscore': 'libs/underscore', //core
     'backbone': 'libs/backbone', //core
     'bootstrap': 'libs/bootstrap', //carousel
     'leaflet': 'libs/leaflet-src', //gis
     'esri.leaflet' : 'libs/esri-leaflet-src',//gis
-    'leaflet.rrose' : 'libs/leaflet.rrose-src',//gis    
-    'leaflet.markercluster' : 'libs/leaflet.markercluster',//gis        
-    'topojson' : 'libs/topojson',//gis
     'templates': 'templates',//core
     "ga": "//www.google-analytics.com/analytics"    
   },    
